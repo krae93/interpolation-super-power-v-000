be8ad5feef3249058a540f8bc514c3437d85338d
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-26 02:24:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,14 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  display_rainbow.prints.out.the.colors.of.the.rainbow.correctly.when.passed.in.order = ("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n")
+  puts "colors: #{colors[0]}"
+end
+
+def colors
+  colors[0] = "red"
+  colors[1] = "orange"
+  colors[2] = "yellow"
+  colors[3] = "green"
+  colors[4] = "blue"
+  colors[5] = "indigo"
+  colors[6] = "violet"
+end
