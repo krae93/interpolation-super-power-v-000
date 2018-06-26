54aee50ee44bd48893d98d19745c2fdd46828e0f
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-26 03:20:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,24 +1,3 @@
-#def display_rainbow(colors)
-#  puts "colors: #{colors[0]}"
-#  puts "colors: #{colors[1]}"
-#  puts "colors: #{colors[2]}"
-#  puts "colors: #{colors[3]}"
-#  puts "colors: #{colors[4]}"
-#  puts "colors: #{colors[5]}"
-#  puts "colors: #{colors[6]}"
-#end
-
-#def colors
-#  colors[0] = "red"
-#  colors[1] = "orange"
-#  colors[2] = "yellow"
-#  colors[3] = "green"
-#  colors[4] = "blue"
-#  colors[5] = "indigo"
-#  colors[6] = "violet"
-#display_rainbow(colors)= "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n"
-#end
-
 def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
